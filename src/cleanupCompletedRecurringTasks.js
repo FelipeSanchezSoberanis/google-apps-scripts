ac320f5f18c2dd052fb21570a3ef2c77231885ab
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2026-08-19 13:01:42 -0600

    feat: max results and max due date

--- src/cleanupCompletedRecurringTasks.js
+++ src/cleanupCompletedRecurringTasks.js
@@ -1,12 +1,14 @@
 /**
- * @param {(task: GoogleAppsScript.Tasks.Schema.Task & { taskListId: string }, taskIdx: number) => boolean} filter
+ * @param {Object} arg0
+ * @param {(task: GoogleAppsScript.Tasks.Schema.Task & { taskListId: string }, taskIdx: number) => boolean} arg0.filter
+ * @param {Object} arg0.options
  */
-const getTasksWith = (filter) => {
+const getTasksWith = ({ filter, options }) => {
   return Tasks?.Tasklists.list()
     .items?.flatMap(({ id: taskListId }) => {
       if (!taskListId) return [];
       return (
-        Tasks?.Tasks.list(taskListId, { showHidden: true }).items?.map((task) => ({
+        Tasks?.Tasks.list(taskListId, options).items?.map((task) => ({
           ...task,
           taskListId,
         })) || []
@@ -16,13 +18,18 @@ const getTasksWith = (filter) => {
 };
 
 const cleanupCompletedRecurringTasks = () => {
+  const tomorrow = new Date();
+  tomorrow.setDate(tomorrow.getDate() + 1);
+
   const yesterday = new Date();
   yesterday.setDate(yesterday.getDate() - 1);
-  getTasksWith(({ status, due }) => status === "completed" && new Date(due) < yesterday).forEach(
-    (task) => {
-      if (!task.id) return;
-      Logger.log(`Removing task:\n${JSON.stringify(task)}`);
-      Tasks?.Tasks.remove(task.taskListId, task.id);
-    },
-  );
+
+  getTasksWith({
+    options: { showHidden: true, dueMax: tomorrow.toISOString(), maxResults: 100 },
+    filter: ({ status, due }) => status === "completed" && new Date(due) < yesterday,
+  }).forEach((task) => {
+    if (!task.id) return;
+    Logger.log(`Removing task:\n${JSON.stringify(task)}`);
+    Tasks?.Tasks.remove(task.taskListId, task.id);
+  });
 };
